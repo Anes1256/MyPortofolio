aab6b2ac5def14ac42c6327ab9eb0c456d76c0c8
Anes1256 <122750568+Anes1256@users.noreply.github.com>
2025-03-10 17:30:46 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function scrollfun(){
+    document.getElementById("html_card").scrollIntoView({behavior: "smooth"})
+}
+function fb(){
+    open("https://www.facebook.com/profile.php?id=61554801214926")
+}
